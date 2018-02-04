59c53f76de3ef6f02578417b8eb6e82e24aac9fa
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-02-04 10:41:12 +0500

    update tests and fixtures

--- test/controllers/web/moderation/articles_controller_test.rb
+++ test/controllers/web/moderation/articles_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class Web::Moderation::ArticlesControllerTest < ActionDispatch::IntegrationTest
   setup do
     @article = articles(:on_moderate)
+    @category = article_categories(:default)
   end
 
   test 'should get index' do
@@ -24,7 +25,14 @@ class Web::Moderation::ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'patch update' do
-    patch moderation_article_url(@article), params: { article: { state_event: 'publish' } }
+    params = {
+      article: {
+        state_event: 'publish',
+        category_id: @category
+      }
+    }
+
+    patch moderation_article_url(@article), params: params
 
     assert { @article.reload.published? }
   end
